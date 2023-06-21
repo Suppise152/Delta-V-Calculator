59bb4d59d69ac25bec60e6226a026a1770561266
timC <tCas362@aucklanduni.ac.nz>
2023-06-21 22:06:13 +1200

    fixed logic for calculating sum

--- SAC-1.js
+++ SAC-1.js
@@ -2,55 +2,29 @@ function changeImage(imagePath) {
     const calculatorImage = document.getElementById('calculator-image');
     calculatorImage.src = imagePath;
   }
-  
-  // Calculate the sum based on branch and node selection
-function calculateSum() {
-  const display = document.getElementById('display');
-  const toggle1 = document.getElementById('toggle1');
-  const toggle2 = document.getElementById('toggle2');
 
-  const branches = document.getElementsByClassName('branch');
-
+// Toggle the selected state of a node within a branch
+function calculateSum(branchId, nodeIndex) {
+  const branch = document.getElementById(branchId);
+  const nodes = branch.getElementsByClassName('node');
   let sum = 10;
 
-  // Calculate sum for each branch
-  for (let i = 0; i < branches.length; i++) {
-    const branch = branches[i];
-    const activeNodes = branch.getElementsByClassName('active');
-
-    // Calculate sum for active nodes in the branch
-    for (let j = 0; j < activeNodes.length; j++) {
-      const node = activeNodes[j];
-      const nodeValue = parseInt(node.dataset.value);
-      sum += nodeValue;
+  // sums all the nodes in the branch
+  for (let i = 0; i <= nodeIndex; i++) {
+    if (toggle1.checked && i === nodes.length - 1) {
+      break;
     }
+    const node = nodes[i];
+    const nodeValue = parseInt(node.dataset.value);
+    sum += nodeValue;
   }
 
   if (toggle2.checked) {
     sum *= 2; // Double the sum if Toggle 2 is checked
+    if (toggle1.checked) {
+      sum -= 10; // Subtract base value if Toggle 2 is checked
+    }
   }
 
   display.value = sum; // Update the display with the calculated sum
 }
-
-// Toggle the selected state of a node within a branch
-function toggleNode(branchId, nodeIndex) {
-
-  const branch = document.getElementById(branchId);
-  const nodes = branch.getElementsByClassName('node');
-
-  for (let i = 0; i < nodes.length; i++) {
-    if (i <= nodeIndex) {
-      nodes[i].classList.add('active'); // Add the "active" class to selected node
-    } else {
-      nodes[i].classList.remove('active'); // Remove the "active" class from remaining nodes
-    }
-    if (toggle1.checked && i == nodes.length - 1) {
-      nodes[i].classList.remove('active'); // Remove the "active" class from the last node if Toggle 1 is checked
-    }
-  }
-
-  calculateSum(); // Recalculate the sum based on the updated node selection
-}
-
-  
