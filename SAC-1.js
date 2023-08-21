3a828415afe9d05e520ca913f9f4c0c8f21d8577
Suppise152 <tcas362@aucklanduni.ac.nz>
2023-08-21 16:29:16 +1200

    made slider better

--- SAC-1.js
+++ SAC-1.js
@@ -30,6 +30,62 @@ function transferAnlgeImageChange(branchId, direction){
 }
 
 /**
+ * Handles slider changes, and recalculates the sum.
+ */
+const slider = document.getElementById('slider');
+const sliderValue = document.getElementById('slider-value');
+
+// Set the initial value and update the value text
+slider.value = 0;
+
+// Add an input event listener to the slider
+slider.addEventListener('input', function() {
+  handleSliderChange(slider); // Call your calculation function here with the slider value
+});
+
+
+/**
+ * Handles all changes needed for the given slider, and recalculates the sum.
+ * 
+ * @param {*} slider 
+ */
+function handleSliderChange(slider) {
+  const display = document.getElementById('slider-value');
+
+  switch (slider.value) {
+    case '0':
+      redundancy = 1;
+      display.textContent = `+  0% Redundancy`;
+      break;
+    case '1':
+      redundancy = 1.1;
+      display.textContent = `+  10% Redundancy`;
+      break;
+    case '2':
+      redundancy = 1.15;
+      display.textContent = `+  15% Redundancy`;
+      break;
+    case '3':
+      redundancy = 1.25;
+      display.textContent = `+  25% Redundancy`;
+      break;
+    case '4':
+      redundancy = 1.35;
+      display.textContent = `+  35% Redundancy`;
+      break;
+    case '5':
+      redundancy = 1.5;
+      display.textContent = `+  50% Redundancy`;
+      break;
+    default:
+      redundancy = 1;
+      display.textContent = `+  0% Redundancy`;
+      break;
+  }
+  calculateSum(prevBranch, prevNode);
+}
+
+/**
  * Handles all changes needed for the given checkbox, and recalculates the sum.
  * 
  * @param {*} checkbox The checkbox that was changed.
@@ -120,55 +176,6 @@ function handleToggleChange(checkbox) {
     }
 
   //update the dV
-  calculateSum(prevBranch, prevNode);
-}
-
-/**
- * Sets the slider to the default value (0).
- */
-function setDefaultSliderValue() {
-  const slider = document.getElementById('slider');
-  slider.value = 0;
-}
-
-/**
- * Handles all changes needed for the given slider, and recalculates the sum.
- * 
- * @param {*} slider 
- */
-function handleSliderChange(slider) {
-  const display = document.getElementById('slider-value');
-
-  switch (slider.value) {
-    case '0':
-      redundancy = 1;
-      display.textContent = `+  0% Redundancy`;
-      break;
-    case '1':
-      redundancy = 1.1;
-      display.textContent = `+  10% Redundancy`;
-      break;
-    case '2':
-      redundancy = 1.15;
-      display.textContent = `+  15% Redundancy`;
-      break;
-    case '3':
-      redundancy = 1.25;
-      display.textContent = `+  25% Redundancy`;
-      break;
-    case '4':
-      redundancy = 1.35;
-      display.textContent = `+  35% Redundancy`;
-      break;
-    case '5':
-      redundancy = 1.5;
-      display.textContent = `+  50% Redundancy`;
-      break;
-    default:
-      redundancy = 1;
-      display.textContent = `+  0% Redundancy`;
-      break;
-  }
   calculateSum(prevBranch, prevNode);
 }
 
